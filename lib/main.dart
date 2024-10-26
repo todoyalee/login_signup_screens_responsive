797632d60c30db91f7cbf8b09162275492c107a6
Mohamed Ali Belkouri <medali.b@devnet.me>
2024-10-26 23:04:34 +0100

    feat:completing main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 import 'package:login_signup_screens_responsive/views/signUp.dart';
 
 void main() {
-  GetMaterialApp(
-    debugShowCheckedModeBanner: false,
-    
-    
-    home: Signup());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+//* Todoyalee on Github
+//* https://github.com/todoyalee
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignUpView(),
+    );
+  }
 }
